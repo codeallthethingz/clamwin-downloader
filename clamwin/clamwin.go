cd249ee98f77ae382c1fc6fa26152086d0e38042
codeallthethingz <howdoicontactwill@gmail.com>
2020-09-14 16:24:02 -0700

    added in hsb file format

--- clamwin/clamwin.go
+++ clamwin/clamwin.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 func NewClamwinConnector() *ClamwinConnector {
@@ -18,6 +20,7 @@ func NewClamwinConnector() *ClamwinConnector {
 			}
 			return res.Body, nil
 		},
+		BufferSize: 32 * 1024,
 	}
 }
 
@@ -25,6 +28,7 @@ type ClamwinConnectorImpl struct{}
 
 type ClamwinConnector struct {
 	GetClamwinStream func() (io.ReadCloser, error)
+	BufferSize       int32
 }
 
 func (c *ClamwinConnector) Download(out io.Writer) error {
@@ -34,15 +38,43 @@ func (c *ClamwinConnector) Download(out io.Writer) error {
 	}
 	defer in.Close()
 	io.CopyN(ioutil.Discard, in, 512)
-	return ExtractMainMDB(in, out)
+	out = NewClamwinNormalizer(out)
+	return c.ExtractMainMDB(in, out)
 }
 
-func ExtractMainMDB(gzipStream io.Reader, out io.Writer) error {
+func NewClamwinNormalizer(out io.Writer) io.Writer {
+	return &ClamwinNormalizer{
+		Out: out,
+	}
+}
+
+type ClamwinNormalizer struct {
+	Out         io.Writer
+	DanglingBit string
+}
+
+func (w *ClamwinNormalizer) Write(p []byte) (n int, err error) {
+	regex, _ := regexp.Compile(`\b([^:]+):([0-9]+):`)
+	content := string(p)
+	lastReturn := strings.LastIndex(content, "\n")
+	lines := strings.Split(w.DanglingBit+content[0:lastReturn], "\n")
+	newContent := ""
+	for _, line := range lines {
+		newContent += regex.ReplaceAllString(line, "$2:$1:") + "\n"
+	}
+	w.DanglingBit = content[lastReturn+1:]
+	_, err = w.Out.Write([]byte(newContent))
+	return len([]byte(p)), err
+}
+
+func (c *ClamwinConnector) ExtractMainMDB(gzipStream io.Reader, out io.Writer) error {
 	uncompressedStream, err := gzip.NewReader(gzipStream)
 	if err != nil {
 		return err
 	}
 	tarReader := tar.NewReader(uncompressedStream)
+	count := 0
+	buf := make([]byte, c.BufferSize)
 	for true {
 		header, err := tarReader.Next()
 		if err == io.EOF {
@@ -54,12 +86,22 @@ func ExtractMainMDB(gzipStream io.Reader, out io.Writer) error {
 		switch header.Typeflag {
 		case tar.TypeReg:
 			if header.Name == "main.mdb" {
-				if _, err := io.Copy(out, tarReader); err != nil {
+				fmt.Println("\n" + header.Name)
+				if _, err := io.CopyBuffer(out, tarReader, buf); err != nil {
 					return err
 				}
-				return nil
+				count++
+			} else if header.Name == "main.hsb" {
+				fmt.Println("\n" + header.Name)
+				if _, err := io.CopyBuffer(out, tarReader, buf); err != nil {
+					return err
+				}
+				count++
 			}
 		}
 	}
-	return fmt.Errorf("didn't find main.mdb in this tar.gz")
+	if count == 2 {
+		return nil
+	}
+	return fmt.Errorf("didn't find main.mdb and/or main.hsb in this tar.gz")
 }
